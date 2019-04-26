38388086e6ed36bdd84a8dc4d118a40cf41fc31f
Lucian-jw <1063564658@qq.com>
2019-04-26 21:07:45 +0800

    Final vision

--- src/main/java/comp3111/coursescraper/Course.java
+++ src/main/java/comp3111/coursescraper/Course.java
@@ -115,6 +115,124 @@ public class Course {
 		this.commoncore= commoncore;
 	}
 	
+	public Boolean containsAM(){
+		int i = 0;
+		if (this.getNumSlots() == 0) {// If it does not have any slots
+			return false;
+		}
+		while (!this.getSlot(i).isAM()) {
+			if (i == this.getNumSlots() - 1) {
+				return false;
+			}
+			i++;
+		}
+		return true;
+	}
+	public Boolean containsPM(){
+		int i = 0;
+		if (this.getNumSlots() == 0) {// If it does not have any slots
+			return false;
+		}
+		while (!this.getSlot(i).isPM()) {
+			if (i == this.getNumSlots() - 1) {
+				return false;
+			}
+			i++;
+		}
+		return true;
+	}
+	public Boolean containsMon(){
+		int i = 0;
+		if (this.getNumSlots() == 0) { // If it does not have any slots
+			return false;
+		}
+		while (!(this.getSlot(i).getDay() == 0)) { // 0 means Monday,1,2,3,4,5 means Tue....
+			if (i == this.getNumSlots() - 1) {
+				return false;
+			}
+			i++;
+		}
+		return true;
+	}
+	public Boolean containsTue(){
+		int i = 0;
+		if (this.getNumSlots() == 0) { // If it does not have any slots
+			return false;
+		}
+		while (!(this.getSlot(i).getDay() == 1)) { // 0 means Monday,1,2,3,4,5 means Tue....
+			if (i == this.getNumSlots() - 1) {
+				return false;
+			}
+			i++;
+		}
+		return true;
+	}
+	public Boolean containsWed(){
+		int i = 0;
+		if (this.getNumSlots() == 0) { // If it does not have any slots
+			return false;
+		}
+		while (!(this.getSlot(i).getDay() == 2)) { // 0 means Monday,1,2,3,4,5 means Tue....
+			if (i == this.getNumSlots() - 1) {
+				return false;
+			}
+			i++;
+		}
+		return true;
+	}
+	public Boolean containsThurs(){
+		int i = 0;
+		if (this.getNumSlots() == 0) { // If it does not have any slots
+			return false;
+		}
+		while (!(this.getSlot(i).getDay() == 3)) { // 0 means Monday,1,2,3,4,5 means Tue....
+			if (i == this.getNumSlots() - 1) {
+				return false;
+			}
+			i++;
+		}
+		return true;
+	}
+	public Boolean containsFri(){
+		int i = 0;
+		if (this.getNumSlots() == 0) { // If it does not have any slots
+			return false;
+		}
+		while (!(this.getSlot(i).getDay() == 4)) { // 0 means Monday,1,2,3,4,5 means Tue....
+			if (i == this.getNumSlots() - 1) {
+				return false;
+			}
+			i++;
+		}
+		return true;
+	}
+	public Boolean containsSat(){
+		int i = 0;
+		if (this.getNumSlots() == 0) { // If it does not have any slots
+			return false;
+		}
+		while (!(this.getSlot(i).getDay() == 5)) { // 0 means Monday,1,2,3,4,5 means Tue....
+			if (i == this.getNumSlots() - 1) {
+				return false;
+			}
+			i++;
+		}
+		return true;
+	}
+	public Boolean containsLab(){
+		int i = 0;
+		if (this.getNumSlots() == 0) { // If it does not have any slots
+			return false;
+		}
+		while (this.getSlot(i).getSectionType() == null || (!(this.getSlot(i).getSectionType().startsWith("L"))
+				&& !(this.getSlot(i).getSectionType().startsWith("T")))) { // 0 means Monday,1,2,3,4,5 means Tue....
+			if (i == this.getNumSlots() - 1) {
+				return false;
+			}
+			i++;
+		}
+		return true;
+	}
 	
 
 }
--- src/test/java/comp3111/coursescraper/ItemTest.java
+++ src/test/java/comp3111/coursescraper/ItemTest.java
@@ -49,9 +49,107 @@ public class ItemTest {
 		assertEquals(k.isPM(),true);
 	}
 	@Test
-	public void testType(){
+	public void testSection(){
+		Section i=new Section();
+		i.setCourseCode("COMP1021");
+		i.setSection("L1");
+		i.setCourseName("Python");
+		i.setInstructor("Gibson");
+		i.setEnrolledStatus(false);
+		assertEquals(i.getEnrolledStatus(),false);
+		assertEquals(i.getCourseCode(),"COMP1021");
+		assertEquals(i.getCourseName(),"Python");
+		assertEquals(i.getInstructor(),"Gibson");
+		assertEquals(i.getSection(),"L1");
+	}
+	@Test
+	public void testCourseAMPM(){
+		Slot i=new Slot();
+		Slot j=new Slot();
+		Slot k=new Slot();
+		i.setStart("08:00AM");
+		i.setEnd("10:00AM");
+		j.setStart("09:00AM");
+		j.setEnd("11:00AM");
+		k.setStart("12:00PM");
+		k.setEnd("04:00PM");
+		Course a=new Course();
+		a.addSlot(i);
+		a.addSlot(j);
+		assertEquals(a.containsAM(),true);
+		assertEquals(a.containsPM(),false);
+		Course b=new Course();
+		b.addSlot(k);
+		b.addSlot(j);
+		assertEquals(b.containsAM(),true);
+		assertEquals(b.containsPM(),true);
+	}
+	
+	@Test
+	public void testWeekday(){
+		Slot i1=new Slot();
+		Slot i2=new Slot();
+		Slot i3=new Slot();
+		Slot i4=new Slot();
+		Slot i5=new Slot();
+		Slot i6=new Slot();
+		i1.setDay(0);
+		i2.setDay(1);
+		i3.setDay(2);
+		i4.setDay(3);
+		i5.setDay(4);
+		i6.setDay(5);
+		Course a=new Course();
+		a.addSlot(i1);
+		a.addSlot(i2);
+		a.addSlot(i3);
+		Course b=new Course();
+		b.addSlot(i4);
+		b.addSlot(i5);
+		b.addSlot(i6);
+		assertEquals(a.containsMon(),true);
+		assertEquals(a.containsTue(),true);
+		assertEquals(a.containsWed(),true);
+		assertEquals(a.containsThurs(),false);
+		assertEquals(a.containsFri(),false);
+		assertEquals(a.containsSat(),false);
+		assertEquals(b.containsMon(),false);
+		assertEquals(b.containsTue(),false);
+		assertEquals(b.containsWed(),false);
+		assertEquals(b.containsThurs(),true);
+		assertEquals(b.containsFri(),true);
+		assertEquals(b.containsSat(),true);
 		
 	}
+	@Test
+	public void testType(){
+		Slot i1=new Slot();
+		Slot i2=new Slot();
+		Slot i3=new Slot();
+		Slot i4=new Slot();
+		Slot i5=new Slot();
+		Slot i6=new Slot();
+		i1.setSectionType("L1");
+		i2.setSectionType("L2");
+		i3.setSectionType("T1");
+		i4.setSectionType("R1");
+		i5.setSectionType("LA1");
+		i6.setSectionType("X1");
+		Course a=new Course();
+		a.addSlot(i1);
+		a.addSlot(i2);
+		a.addSlot(i3);
+		Course b=new Course();
+		b.addSlot(i4);
+		b.addSlot(i5);
+		b.addSlot(i6);
+		assertEquals(a.containsLab(),true);
+		assertEquals(b.containsLab(),true);
+		
+		
+	}
+	
+	
 	
 	
 	

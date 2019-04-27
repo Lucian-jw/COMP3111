044cb4f7c3645b6867a7a22c1c855838f2d9fb0b
Luoshu Zhang <lzhangbk@ust.hk>
2019-04-27 14:46:02 +0800

    Updated a new version to deal with the difficulty in user interaction.

--- src/main/java/comp3111/coursescraper/Section.java
+++ src/main/java/comp3111/coursescraper/Section.java
@@ -6,110 +6,146 @@ import javafx.beans.property.BooleanProperty;
 import javafx.beans.property.SimpleBooleanProperty;
 import javafx.beans.property.SimpleStringProperty;
 import javafx.beans.property.StringProperty;
+import javafx.scene.control.*;
+import javafx.scene.paint.Color;
 
+@SuppressWarnings("restriction")
 public class Section {
     private SimpleStringProperty CourseCode;
+    
     private SimpleStringProperty Section;
+    
     private SimpleStringProperty Instructor;
+    
     private SimpleStringProperty CourseName;
+    
     private SimpleBooleanProperty EnrolledStatus;
+    
     private final ArrayList<Slot> belongedSlots = new ArrayList<>();
+    
+    private ArrayList<Label> labels = new ArrayList<Label>();
+    
+    private ArrayList<Color> colors = new ArrayList<Color>();
+    
+    public void addColor(Color color) {
+    	colors.add(color);
+    }
+    
+    public int getNumColor() {
+    	return colors.size();
+    }
+    
+    public Color getColor(int i) {
+    	return colors.get(i);
+    }
 
     public void addSlot(final Slot s) {
-	belongedSlots.add(s);
+    	belongedSlots.add(s);
     }
 
     @Override
     public Section clone() {
-	final Section s = new Section();
-	s.setCourseCode(getCourseCode());
-	s.setCourseName(getCourseName());
-	s.setInstructor(getInstructor());
-	s.setSection(getSection());
-	s.setEnrolledStatus(getEnrolledStatus());
-	return s;
+		final Section s = new Section();
+		s.setCourseCode(getCourseCode());
+		s.setCourseName(getCourseName());
+		s.setInstructor(getInstructor());
+		s.setSection(getSection());
+		s.setEnrolledStatus(getEnrolledStatus());
+		return s;
+    }
+    
+    public int getNumLabels() {
+    	return labels.size();
+    }
+    
+    public void addLabel(Label label) {
+    	labels.add(label);
+    }
+    
+    public Label getLabel(int i) {
+    	return labels.get(i);
     }
 
     public StringProperty CourseCodeProperty() {
-	return CourseCode;
+    	return CourseCode;
     }
 
     public StringProperty CourseNameProperty() {
-	return CourseName;
+    	return CourseName;
     }
 
     public BooleanProperty EnrolledStatusProperty() {
-	return EnrolledStatus;
+    	return EnrolledStatus;
     }
 
     public boolean equals(final Section s) {
-	return getCourseCode().equals(s.getCourseCode()) && getSection().equals(s.getSection());
+    	return getCourseCode().equals(s.getCourseCode()) && getSection().equals(s.getSection());
     }
 
     public String getCourseCode() {
 
-	return CourseCode.get();
+    	return CourseCode.get();
 
     }
 
     public String getCourseName() {
 
-	return CourseName.get();
+    	return CourseName.get();
 
     }
 
     public Boolean getEnrolledStatus() {
 
-	return EnrolledStatus.get();
+    	return EnrolledStatus.get();
 
     }
 
     public String getInstructor() {
 
-	return Instructor.get();
+    	return Instructor.get();
 
     }
 
     public String getSection() {
 
-	return Section.get();
+    	return Section.get();
 
     }
 
     public Slot getSlot(final int i) {
-	return belongedSlots.get(i);
+    	return belongedSlots.get(i);
     }
 
     public int getSlotSize() {
-	return belongedSlots.size();
+    	return belongedSlots.size();
     }
 
     public StringProperty InstructorProperty() {
-	return Instructor;
+    	return Instructor;
     }
 
     public StringProperty SectionProperty() {
-	return Section;
+    	return Section;
     }
 
     public void setCourseCode(final String CCode) {
-	CourseCode = new SimpleStringProperty(CCode);
+    	CourseCode = new SimpleStringProperty(CCode);
     }
 
     public void setCourseName(final String CName) {
-	CourseName = new SimpleStringProperty(CName);
+    	CourseName = new SimpleStringProperty(CName);
     }
 
     public void setEnrolledStatus(final Boolean status) {
-	EnrolledStatus = new SimpleBooleanProperty(status);
+    	EnrolledStatus = new SimpleBooleanProperty(status);
     }
 
     public void setInstructor(final String Instructor) {
-	this.Instructor = new SimpleStringProperty(Instructor);
+    	this.Instructor = new SimpleStringProperty(Instructor);
     }
 
     public void setSection(final String section) {
-	Section = new SimpleStringProperty(section);
+    	Section = new SimpleStringProperty(section);
     }
 
 }
